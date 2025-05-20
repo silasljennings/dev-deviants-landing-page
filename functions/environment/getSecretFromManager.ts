721177d81791a7bd7907d61b6b925d5006ffe714
silasljennings <silasljennings@gmail.com>
2025-05-19 22:12:20 -0500

    remove secret from commit

--- functions/environment/getSecretFromManager.ts
+++ functions/environment/getSecretFromManager.ts
@@ -1,18 +1,21 @@
 import { SecretManagerServiceClient } from "@google-cloud/secret-manager";
-import { app } from "../../startup/apps";
+import {adminApp} from "../src";
 
 const secretManagerClient = new SecretManagerServiceClient();
 
 export const getSecretFromManager = async (secretName: string): Promise<string> => {
     try {
-        // Access the latest version of the secret
-        const projectId = app.options.projectId
-        console.log("getSecretFromManager", projectId);
+        const projectId = adminApp.options.projectId
         const [accessResponse] = await secretManagerClient.accessSecretVersion({
             name: `projects/${projectId}/secrets/${secretName}/versions/latest`,
         });
+
         const secret = accessResponse.payload?.data?.toString();
-        if (!secret) { throw new Error("Secret payload is empty or undefined."); }
+
+        if (!secret) {
+            throw new Error("Secret payload is empty or undefined.");
+        }
+
         return secret;
     } catch (error) {
         console.error("Error accessing secret:", error);
